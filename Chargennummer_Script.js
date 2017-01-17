b6ee4be0dcd176ab1fc87cb0ba0455ffce0d9e47
Ronald Fortmann Jr <A-Sm1th@users.noreply.github.com>
2017-01-17 07:54:27 +0100

    Add files via upload

---
+++ Chargennummer_Script.js
@@ -0,0 +1,71 @@
+var myDialog = app.dialogs.add({name:"Chargennummer Eingabe"});
+//Add a dialog column.
+with(myDialog.dialogColumns.add()){
+  staticTexts.add({staticLabel:"Chargennummer:"});
+  staticTexts.add({staticLabel:"Artikel-Nr:"});
+  staticTexts.add({staticLabel:"Produktname:"});
+  staticTexts.add({staticLabel:"Ankunft Datum:"});
+}
+with(myDialog.dialogColumns.add()){
+  var Chargennummer = textEditboxes.add({minWidth:100});
+  var Artikel_Nr = textEditboxes.add({minWidth:100});
+  var Produktname = textEditboxes.add({minWidth:100});
+  var Datum = textEditboxes.add({editContents:"TT.MM.YYYY"},{minWidth:100});
+}
+//Show the dialog box.
+var myResult = myDialog.show();
+//If the user clicked OK, display one message;
+//if they clicked Cancel, display a different message.
+if(myResult == true){
+alert("Chargennummer " + Chargennummer.editContents + ",Artikel-Nr. " +
+ Artikel_Nr.editContents + " und Produkt Name " + Produktname.editContents +
+ " wurde eingegeben.");
+
+
+//Remove the dialog box from memory.
+
+
+//Show the dialog box.
+
+var myDocument = app.activeDocument;
+//Clear the find/change text preferences.
+  app.findTextPreferences = NothingEnum.nothing;
+  app.changeTextPreferences = NothingEnum.nothing;
+//Set the find options.
+//Search the document for the string "copy" and change it to "text".
+    app.findTextPreferences.findWhat = "Chargennummer";
+    app.changeTextPreferences.changeTo = Chargennummer.editContents;
+
+  myDocument.changeText();
+//Clear the find/change text preferences after the search.
+  app.findTextPreferences = NothingEnum.nothing;
+  app.changeTextPreferences = NothingEnum.nothing;
+
+    app.findTextPreferences.findWhat = "Artikel-Nr";
+    app.changeTextPreferences.changeTo = Artikel_Nr.editContents;
+
+  myDocument.changeText();
+
+  app.findTextPreferences = NothingEnum.nothing;
+  app.changeTextPreferences = NothingEnum.nothing;
+
+    app.findTextPreferences.findWhat = "Produkt Name";
+    app.changeTextPreferences.changeTo = Produktname.editContents;
+
+  myDocument.changeText();
+
+  app.findTextPreferences = NothingEnum.nothing;
+  app.changeTextPreferences = NothingEnum.nothing;
+
+    app.findTextPreferences.findWhat = "Datum";
+    app.changeTextPreferences.changeTo = Datum.editContents;
+
+  myDocument.changeText();
+
+  app.findTextPreferences = NothingEnum.nothing;
+  app.changeTextPreferences = NothingEnum.nothing;
+}
+else{
+    alert("Der Prozess wurde gecancelt.")
+}
+myDialog.destroy();
